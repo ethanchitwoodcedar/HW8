c6f8c83b9c301c03339eb689fbe948fc803e8a24
Trenton271 <tmeier@cedarville.edu>
2025-04-03 20:11:08 -0400

    Wire class ver 1.0

--- src/wire.cpp
+++ src/wire.cpp
@@ -1,9 +1,44 @@
 #include "Wire.h"
 
-Wire::Wire(/* args */)
+Wire::Wire(int v = 0, char n = '-', int i = 0)
 {
+    value = v;
+    name = n;
+    index = i;
+
+}
+
+int Wire::getValue() {
+    return value;
+}
+
+char Wire::getName() {
+    return name;
+}
+
+int Wire::getIndex() {
+    return index;
+}
+
+void Wire::setValue(int v) {
+    value = v;
+}
+
+void Wire::setName(char n) {
+    name = n;
+}
+
+void Wire::setIndex(int i) {
+    index = i;
+}
+
+void Wire::printHistory() {
+    for (int i = 0; i < history.size(); i++) {
+        std::cout << history.at(i);
+    }
 }
 
 Wire::~Wire()
 {
+    delete this;
 }
--- src/wire.h
+++ src/wire.h
@@ -1,14 +1,27 @@
 #ifndef WIRE_H
 #define WIRE_H
+#include <vector>
+#include <iostream>
 
 class Wire
 {
 private:
     int value;
+    char name;
+    //string name;
+    std::vector<Gate*> drives;
+    int index;
+    std::vector<int> history;
     /* data */
 public:
-    Wire(/* args */);
-    int getValue() {return value;}
+    Wire(int v, char n, int i);
+    int getValue(); //{return value;}
+    char getName();
+    int getIndex();
+    void setValue(int v);
+    void setName(char n);
+    void setIndex(int i);
+    void printHistory();
     ~Wire();
 };
 
